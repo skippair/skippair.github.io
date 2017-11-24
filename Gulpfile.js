ca4c9c12d56cd32b00da9e92afd5a09c63033334
Bastoon <bastien.merceron@gmail.com>
2017-11-24 18:12:10 +0100

    Update templates
    update gulp
    update bootstrap components

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,19 +5,18 @@ var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
 var runsequence = require('run-sequence');
 
-/*
 gulp.task('js', function () {
   return gulp.src([
-    './node_modules/bootstrap/js/src/index.js'
+    './node_modules/bootstrap/js/src/collapse.js',
+    './node_modules/bootstrap/js/src/util.js'
   ])
       .pipe(gulp.dest('./assets/js/bootstrap'));
 });
-*/
 
 gulp.task('sass', function () {
   return gulp.src([
     './assets/scss/main.scss'
-  //    another sccs files to compile
+    //    another sccs files to compile
   ])
       .pipe(sass().on('error', sass.logError))
       .pipe(csso())
@@ -26,17 +25,32 @@ gulp.task('sass', function () {
       .pipe(gulp.dest('./assets/css'));
 });
 
+gulp.task('img', function () {
+  return gulp.src([
+    './assets/img/*.+(png|jpg|gif|svg|jpeg)'
+  ])
+      .pipe(gulp.dest('./assets/img'));
+});
+
 gulp.task('watch', function () {
   var onChange = function (event) {
     console.log('File ' + event.path + ' has been ' + event.type);
   };
   // Watch Sass files
   gulp.watch([
-    './assets/scss/*.scss'
+    './assets/scss/*.scss',
+    './assets/scss/components/**/*.scss'
   ], ['sass']).on('change', onChange);
   // another files to watch
+  gulp.watch([
+    '.assets/js/bootstrap/*.js',
+    './assets/js/*.js'
+  ], ['js']).on('change', onChange);
+  gulp.watch([
+    '.assets/img/.+(png|jpg|gif|svg|jpeg)'
+  ], ['img']).on('change', onChange);
 });
 
-gulp.task('build', function(callback) {
-  runsequence('sass', callback)
+gulp.task('build', function (callback) {
+  runsequence('sass', 'img', 'js', callback)
 });
